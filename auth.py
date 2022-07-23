6b78e02542059e4c5f71924ea9978ce9b384aa95
amolpsingh <mramol@gmail.com>
2022-07-22 23:45:45 -0700

    update name

--- auth.py
+++ auth.py
@@ -1,10 +1,10 @@
 
 #for getAndProcessTweets.py
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
-bearer_token = ""
+consumer_key = 'GVLc1lspybSq2AJtSkoCOt81s'
+consumer_secret = 'J6NgWZZrwCRN7197d1IY5yzVeIaNcG3YqVDcnY9q267O85maKa'
+access_token = '1132420582379622400-ZKU0F2mATgkTJbbDFcnzbcXAABmwzt'
+access_token_secret = 'X431ky0eiJ5oZ6Y4i1QNl4wWSbnpmnSC7OqCPvxPP7vQK'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAO12fAEAAAAAQm7EFkQSy%2Fsmuc5Ru%2BSduuir7wM%3DDJenkTYX2gbfXokRAswEEjDyanknFmOysXq9py06EPvrdgzbO8'
 
 #for sentimenModelHF.py
-hf_token = ""
+hf_token = "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"
